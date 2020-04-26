2e805323b39a659cc09983bbb1ec07a75f9483b7
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-04-26 11:05:56 -0300

    Configurando propriedades
    Configurando propriedades da aplicação

---
+++ dadosabertos/dadosabertos/src/main/java/com/samuel/dadosabertos/DadosAbertosApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.dadosabertos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosAbertosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosAbertosApplication.class, args);
+	}
+
+}
---
+++ dadosabertos/dadosabertos/src/test/java/com/samuel/dadosabertos/DadosAbertosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.samuel.dadosabertos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DadosAbertosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
